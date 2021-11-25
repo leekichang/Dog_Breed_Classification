9df1015b58903ba5afa2452bc35a71c37c8e46d0
leekichang <1kichang@yonsei.ac.kr>
2021-11-26 07:27:49 +0900

    denseNet161 added have to revise some dataloader part

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Dog_Breed_Classification
 Dog Breed Classification for 2021 Fall IIE4123 Deep Learning and its application lecture.
 
+First of all the requirements for this project is summarized in 'requirements.txt'. Please go check that out.
+
 We used Stanford Dogs Dataset. Download the dataset at http://vision.stanford.edu/aditya86/ImageNetDogs/
 Create 'data' folder and unzip the 'images.tar' and 'annotation.tar' in 'data' folder.
 Run 'dataset_rename.py' with 'python3 dataset_rename.py'.
-This process will revise the folders' name under 'images' and 'annotation' folder.
+This process will revise the folders' name under 'images' and 'annotation' folder. In addition it explicitly split the train data and test data. I splited based on 5 fold and I fixed the random seed to make sure that all team mates are on the same data.
--- dataset_rename.py
+++ dataset_rename.py
@@ -5,7 +5,7 @@ DATASET_DIR = './data/'
 
 
 # %%
-os.chdir(DATASET_DIR+"images")
+os.chdir(DATASET_DIR+"Images")
 for (root, directories, files) in os.walk('./'):
     for d in directories:
         d_path = os.path.join(root, d)
@@ -13,12 +13,36 @@ for (root, directories, files) in os.walk('./'):
         os.rename(d_path, n_path)
         print(n_path)
 
-os.chdir("../annotation/")
+os.chdir("../Annotation/")
 for (root, directories, files) in os.walk('./'):
     for d in directories:
         d_path = os.path.join(root, d)
         n_path = './'+d_path.split('-')[1]
         os.rename(d_path, n_path)
-        print(n_path)        
+        print(n_path)    
+# %%
+os.chdir("../")
+
+# %%
+for (root, directories, files) in os.walk('./Images/'):
+    for d in directories:
+        d_path = os.path.join(root, d)
+        os.mkdir('./test/'+d_path.split('Images/')[1])
 
 
+# %%
+import random as rd
+import shutil
+rd.seed(100)
+
+for (root, directories, files) in os.walk('./Images/'):
+    for d in directories:
+        d_path = os.path.join(root, d)
+        for (root_, directories_, files_) in os.walk(d_path):
+            test_samples = rd.sample(files_, int(len(files_)*0.2))
+            for sample in test_samples:
+                new_path = shutil.move(f"{d_path}/{sample}", './test/'+d)    
+                
+            
+# %%
+os.rename('./Images/', './train')
